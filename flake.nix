45b43c31eb7523f03f4c69091956c90dadb161b9
Andres Navarro Pedregal <andres-nav@users.noreply.github.com>
2026-03-01 10:45:54 +0100

    squash

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "neural networks lab";
+
+  outputs = { self, nixpkgs, }:
+    let system = "x86_64-linux";
+    in {
+      devShells.${system}.default = with import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+        let
+          custom-python = python310.withPackages (p:
+            with p; [
+              ipykernel
+              matplotlib
+              numpy
+              torch
+              torchvision
+              wandb
+              black
+              pycuda
+            ]);
+        in mkShell {
+          packages = [
+            # custom-python
+            # cudaPackages.cuda_nvcc
+
+            # cudaPackages.cuda_cccl
+            # cudaPackages.cudatoolkit
+          ];
+        };
+    };
+}
